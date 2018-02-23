a5ed5b08e51775da4e143605e0171410a961c9cf
johnflame <571748236@qq.com>
2018-02-23 16:05:40 +0800

    实现切换页面暂停动画.

--- js/main.js
+++ js/main.js
@@ -3,21 +3,21 @@ const ctx = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 const fireworks = [];//装烟花的筒
-
-
+let raf; //requestAnimationFrame的返回值，用来暂停播放动画
+let timer; //setInterval的返回值，用来控制烟花生成
 class Firework {
-    constructor(x, y, r, color, destination) {  //烟花的位置，半径，颜色,爆炸的地点，飞行的速度
+    constructor(x, y, r, color, destination) {  //烟花的位置，半径，颜色,爆炸的地点，飞行的速度/帧
         this.isDead = false;
         this.x = x;
         this.y = y;
         this.r = r;
         this.color = color;
         this.destination = destination;
-        this.speedX = (destination.x - x) * 0.03;
-        this.speedY = (destination.y - y) * 0.03;
         this.sparks = [];  //火花
         this.moveX = 0;  //运动距离
-        this.targetX = destination.x - x;
+        this.targetX = destination.x - x; //目标运动距离
+        this.speedX = this.targetX *0.02;
+        this.speedY = (this.destination.y - y)*0.02;
     }
     paint() {
         ctx.save();
@@ -30,13 +30,17 @@ class Firework {
         ctx.fillStyle = "rgba(255,128,150,0.3)";
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.r + 3 * Math.random() + 1, 0, 2 * Math.PI);
+        // ctx.fillRect(this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2)
         ctx.fill();
         ctx.restore();
     }
     move() {                 //移动烟花
-        if (Math.abs(this.moveX) > Math.abs(this.targetX)) {  //如烟花运动距离超过出发点与目标点间的距离
+      
+        if (Math.abs(this.moveX) >= Math.abs(this.targetX)) {  //如烟花运动距离超过出发点与目标点间的距离
             this.isDead = true;
-        } else {   //否则继续匀速运动
+        } else {   //否则继续运动
+            this.speedX = this.speedX * 0.99;
+            this.speedY = this.speedY * 0.99;
             this.x = this.x + this.speedX;
             this.y = this.y + this.speedY;
             this.moveX += this.speedX;
@@ -44,7 +48,7 @@ class Firework {
     }
     
     explode() {
-        const sparkNum = getRandom(100, 200);  //随机产生火花的数量
+        const sparkNum = getRandom(50, 100);  //随机产生火花的数量
         const radius = getRandom(3, 4); //随机产生的火花半径
         for (let i = 0; i < sparkNum; i++) {
             const r = parseInt(getRandom(0,255)),
@@ -71,7 +75,6 @@ function animate() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.restore();
-
     if (fireworks.length) {
         fireworks.forEach(function (firework, index) {
             const that = firework;
@@ -88,32 +91,37 @@ function animate() {
                         }
                     })
                 }
-                // fireworks.splice(index,1);
             } else {
                 firework.paint();
                 firework.move();
             }
         })
     }
-    requestAnimationFrame(animate);
+    raf = requestAnimationFrame(animate);
 }
-
-// canvas.addEventListener('click',function() {
-//     var endX = event.clientX;
-//     var endY = event.clientY;
-//     var fireworks = new Firework(getRandom(canvas.width/6,canvas.width*5/6), canvas.height,2, "#fff", {x: endX, y: endY});
-//     fireworks.push(fireworks)
-//   })
+function createFw(){
+        let startX = getRandom(canvas.width / 6, canvas.width * 5 / 6);
+        let startY = canvas.height;
+        let endX = getRandom(canvas.width / 6, canvas.width * 5 / 6);
+        let endY = getRandom(canvas.height / 5, canvas.height / 4);
+        const firework = new Firework(startX, startY, 8, '#fff', { x: endX, y: endY });
+        fireworks.push(firework);
+}
+window.addEventListener('focus',function(){
+     raf = window.requestAnimationFrame(animate);
+     timer = setInterval(createFw,800);
+})
+window.addEventListener('blur',function(){
+    window.cancelAnimationFrame(raf);
+    window.clearInterval(timer);
+})
+canvas.addEventListener('click',function() {
+    const endX = event.clientX;
+    const endY = event.clientY;
+    const firework = new Firework(getRandom(canvas.width/6,canvas.width*5/6), canvas.height,8, "#fff", {x: endX, y: endY});
+    fireworks.push(firework)
+  })
 document.addEventListener('DOMContentLoaded', function () {
-setInterval(function (){
-    let startX = getRandom(canvas.width / 6, canvas.width * 5 / 6);
-    let startY = canvas.height;
-    let endX = getRandom(canvas.width / 6, canvas.width * 5 / 6);
-    let endY = getRandom(canvas.height / 5, canvas.height / 4);
-    const firework = new Firework(startX, startY, 5, '#fff', { x: endX, y: endY });
-    fireworks.push(firework);
-},500)
-   
-
+    timer = setInterval(createFw,800);
     animate();
 })
